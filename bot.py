a9c221efe7f462f2e4edc7e9cece1ab0caf50731
gali95 <galiach95@gmail.com>
2017-11-12 23:09:11 +0100

    botee

---
+++ bot.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, Response
+
+from kik import KikApi, Configuration
+from kik.messages import messages_from_json, TextMessage
+
+app = Flask(__name__)
+kik = KikApi("rpg.sim", "51f887c9-2538-42b9-b458-411bd383a42f")
+
+kik.set_configuration(Configuration(webhook="localhost"))
+
+@app.route('/incoming', methods=['POST'])
+def incoming():
+    if not kik.verify_signature(request.headers.get('X-Kik-Signature'), request.get_data()):
+        return Response(status=403)
+
+    messages = messages_from_json(request.json['messages'])
+
+    for message in messages:
+        if isinstance(message, TextMessage):
+            kik.send_messages([
+                TextMessage(
+                    to=message.from_user,
+                    chat_id=message.chat_id,
+                    body=message.body
+                )
+            ])
+
+    return Response(status=200)
+
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
