c4ba447fc3be222b6a858be9439450682115c3b0
gali95 <galiach95@gmail.com>
2017-11-13 00:24:00 +0100

    kamaaan man!

--- bot.py
+++ bot.py
@@ -6,9 +6,9 @@ from kik.messages import messages_from_json, TextMessage
 app = Flask(__name__)
 kik = KikApi("rpg.sim", "51f887c9-2538-42b9-b458-411bd383a42f")
 
-kik.set_configuration(Configuration(webhook="http://kik-bot-testingstuff.193b.starter-ca-central-1.openshiftapps.com/incoming"))
+kik.set_configuration(Configuration(webhook="http://kik-bot-testingstuff.193b.starter-ca-central-1.openshiftapps.com/kik-bot"))
 
-@app.route('/incoming', methods=['POST'])
+@app.route('/kik-bot', methods=['POST'])
 def incoming():
     if not kik.verify_signature(request.headers.get('X-Kik-Signature'), request.get_data()):
         return Response(status=403)
